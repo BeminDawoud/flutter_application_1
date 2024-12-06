63fc4c6517434e30ef39ee3df71d6bb4b4059e41
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-12-06 14:17:33 +0200

    organize pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,22 @@
 import 'package:flutter/material.dart';
-import 'drawer.dart';
-import 'card.dart';
+import './pages/home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: const HomePage(),
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      useMaterial3: false,
-      primarySwatch: Colors.deepPurple,
-    ),
-  ));
+  runApp(MyApp());
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  var description = 'This is The Description';
-  TextEditingController _descriptionController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My First Flutter'),
+    return MaterialApp(
+      home: const HomePage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: false,
+        primarySwatch: Colors.deepPurple,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: SingleChildScrollView(
-            child: MyCard(
-                description: description,
-                descriptionController: _descriptionController),
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          description = _descriptionController.text;
-          setState(() {});
-        },
-        child: Icon(Icons.send),
-      ),
-      drawer: MyDrawer(),
     );
   }
 }
---
+++ lib/pages/home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../drawer.dart';
+import '../card.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var description = 'This is The Description';
+  TextEditingController _descriptionController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My First Flutter'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: SingleChildScrollView(
+            child: MyCard(
+                description: description,
+                descriptionController: _descriptionController),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          description = _descriptionController.text;
+          setState(() {});
+        },
+        child: Icon(Icons.send),
+      ),
+      drawer: MyDrawer(),
+    );
+  }
+}
