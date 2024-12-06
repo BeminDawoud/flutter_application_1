73e092e5944546ca33d62074de8f0fac022927cb
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-12-06 12:29:32 +0200

    Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,38 @@ class HomePage extends StatelessWidget {
         onPressed: () {},
         child: Icon(Icons.edit),
       ),
+      drawer: Drawer(
+        child: ListView(
+          children: const [
+            UserAccountsDrawerHeader(
+              accountName: Text('Bemin Dawoud'),
+              accountEmail: Text('Bemin_dawoud@yahoo.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Profile'),
+              subtitle: Text('Bemin Dawoud'),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Profile'),
+              subtitle: Text('Bemin Dawoud'),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Profile'),
+              subtitle: Text('Bemin Dawoud'),
+              trailing: Icon(Icons.edit),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
